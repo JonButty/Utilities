3cf77b60a14cea680322d86c64ed96dd0b769c08
Jonathan Butt <jon_butt33@hotmail.com>
2018-03-01 16:44:25 -0800

    initial commit

---
+++ src/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Utilities
+//
+//  Created by Jonathan Butt on 3/1/18.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
