084dd845d9900fd8eb2aad9ad2286633f489b8b6
BUTT-SEATTLE\Butt <jon_butt33@hotmail.com>
2018-03-08 20:29:13 -0800

    added vld support

--- tests/main-test.h
+++ tests/main-test.h
@@ -3,6 +3,13 @@
 
 #include <gtest/gtest.h>
 
+#if defined(_WIN32) || defined(WIN32)
+#if defined(MEMORY_LEAK)
+#define _DEBUG
+#include <vld.h>
+#endif
+#endif
+
 int main(int argc, char **argv) {
     ::testing::InitGoogleTest(&argc, argv);
     return RUN_ALL_TESTS();
